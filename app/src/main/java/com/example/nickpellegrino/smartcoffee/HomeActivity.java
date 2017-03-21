0181c8be5f957b32c71d4ddc07f6cfc4a31774a3
nickpellegrino <npelleg1@nd.edu>
2017-03-21 15:32:14 -0400

    home activity

---
+++ app/src/main/java/com/example/nickpellegrino/smartcoffee/HomeActivity.java
@@ -0,0 +1,55 @@
+package com.example.nickpellegrino.smartcoffee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    //    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    //    DatabaseReference myRef = database.getReference("Orders");
+
+    //    myRef.setValue("Black Coffee");
+
+        final ImageButton newOrderButton = (ImageButton) findViewById(R.id.newOrderButton);
+        newOrderButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
+
+        final ImageButton historyButton = (ImageButton) findViewById(R.id.historyButton);
+        historyButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
+
+        final ImageButton statusButton = (ImageButton) findViewById(R.id.statusButton);
+        statusButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
+
+        final ImageButton profileButton = (ImageButton) findViewById(R.id.profileButton);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
+
+        final ImageButton imageButton5 = (ImageButton) findViewById(R.id.imageButton5);
+        imageButton5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
+    }
+}
