7d067963ec002c9cb6081f80e79d25cbc73005c5
nickpellegrino <npelleg1@nd.edu>
2017-04-24 20:06:02 -0400

    authentication completed. for testing purposes, use vendor@vendor.com for the email

--- app/src/main/java/com/example/nickpellegrino/smartcoffee/LoginActivity.java
+++ app/src/main/java/com/example/nickpellegrino/smartcoffee/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.nickpellegrino.smartcoffee;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,50 +15,51 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private boolean isVendor;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        database = FirebaseDatabase.getInstance();
         final EditText emailText = (EditText) findViewById(R.id.emailTextField);
         final EditText passwordText = (EditText) findViewById(R.id.passwordTextField);
-
-        Button signUpButton = (Button) findViewById(R.id.signUpButton);
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                createNewAccount(emailText.getText().toString(), passwordText.getText().toString());
-            }
-        });
-        Button logInButton = (Button) findViewById(R.id.logInButton);
-        logInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
+        isVendor = false;
+        myRef = database.getReference().child("Vendors");
         mAuth = FirebaseAuth.getInstance();
+
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
-                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-                    intent.putExtra("UserID", mAuth.getCurrentUser().getUid());
-                    startActivity(intent);
-
+                    new VerifyVendorTask().execute(mAuth.getCurrentUser().getEmail());
                 } else {
                     // User is signed out
                 }
             }
         };
+
+        Button signUpButton = (Button) findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AuthenticateTask().execute(emailText.getText().toString(), passwordText.getText().toString());
+            }
+        });
     }
 
     @Override
@@ -74,24 +76,72 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    public void createNewAccount(String email, String password){
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
+    private class AuthenticateTask extends AsyncTask<String, Void, Void>{
 
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
-                            Toast.makeText(LoginActivity.this, "Sign Up Failed! Try Again", Toast.LENGTH_SHORT).show();
+        @Override
+        protected Void doInBackground(String... params) {
+            mAuth.createUserWithEmailAndPassword(params[0], params[1])
+                    .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            // If sign in fails, display a message to the user. If sign in succeeds
+                            // the auth state listener will be notified and logic to handle the
+                            // signed in user can be handled in the listener.
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, "Sign Up Failed! Try Again", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+            return null;
+        }
+    }
+
+    private class VerifyVendorTask extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            final String email = params[0];
+            myRef = database.getReference().child("Vendors");
+
+            myRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot ds : dataSnapshot.getChildren()){
+                        if (ds.getKey().equals("email") && ds.getValue().equals(email)){
+                            isVendor = true;
+                            Intent intent = new Intent(getApplicationContext(), VendorHomeActivity.class);
+                            intent.putExtra("UserID", mAuth.getCurrentUser().getUid());
+                            startActivity(intent);
                         }
                     }
-                });
-        // [END create_user_with_email]
-    }
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    System.out.println(databaseError.getMessage());
+                }
+            });
+            return isVendor;
+        }
 
-    public void signIn(String email, String password){
-
+        @Override
+        protected void onPostExecute(Boolean result) {
+            if (!result) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                intent.putExtra("UserID", mAuth.getCurrentUser().getUid());
+                startActivity(intent);
+            }
+            else {
+                Intent intent = new Intent(getApplicationContext(), VendorHomeActivity.class);
+                intent.putExtra("UserID", mAuth.getCurrentUser().getUid());
+                startActivity(intent);
+            }
+        }
     }
 }
+
+
---
+++ app/src/main/java/com/example/nickpellegrino/smartcoffee/Vendor.java
@@ -0,0 +1,19 @@
+package com.example.nickpellegrino.smartcoffee;
+
+/**
+ * Created by nickpellegrino on 4/24/17.
+ */
+
+public class Vendor {
+    public String username;
+    public String password;
+
+    public Vendor(){
+
+    }
+
+    public Vendor(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+}
